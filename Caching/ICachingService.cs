a01aa2ab26b5bfd1411761ff361a3a1a7f48c270
Chintan Shah <chintanshah@promactinfo.com>
2024-06-15 15:52:52 +0530

    Add new methods to ICachingService interface
    - Added `using System.Collections.Generic;` to utilize collections.
    - Introduced `GetAllKeys()` method to return all cache keys as a `HashSet<string>`.
    - Implemented `GetAll()` method to fetch all cache entries as an `IDictionary<string, string>`.

--- Caching/ICachingService.cs
+++ Caching/ICachingService.cs
@@ -1,9 +1,13 @@
-﻿namespace Promact.Core.Caching
+﻿using System.Collections.Generic;
+
+namespace Promact.Core.Caching
 {
     public interface ICachingService
     {
         void Set<T>(string key, T value);
         T Get<T>(string key);
         void Remove(string key);
+        HashSet<string> GetAllKeys();
+        IDictionary<string, string> GetAll();
     }
 }
